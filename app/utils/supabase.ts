d9df92f2f8fc65713b6d49d6a56af8bdb70bf9a6
Choco Li <trust2065@gmail.com>
2025-10-01 14:13:19 +1000

    Gen types from supabase

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -2,11 +2,12 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
+import { Database } from '../../database.types';
 
 const supabaseUrl = 'https://nasfoieybsbrlrfsxrzf.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hc2ZvaWV5YnNicmxyZnN4cnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0MTIyNDgsImV4cCI6MjA3Mzk4ODI0OH0.4U157HZts5q2ARrXVjBwup9YiRRjPRiLk-nhMfR7ruM';
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     ...(Platform.OS !== "web" ? { storage: AsyncStorage } : {}),
     autoRefreshToken: true,
